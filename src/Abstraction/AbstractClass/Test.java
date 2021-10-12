ee50dff0803967cf5706b87ce6d85a13bcb4eefb
chayansamanta8@gmail.com <Being@7088>
2021-10-12 09:24:42 +0530

    hi

---
+++ src/Abstraction/AbstractClass/Test.java
@@ -0,0 +1,14 @@
+package Abstraction.AbstractClass;
+
+public abstract class Test {
+    public abstract void m1();// no body in abstract
+    public abstract void m2();
+
+}
+abstract class Subtest extends Test{ // needs to provide abstract keyword in classs or provide implementation of m2.
+    @Override
+    public void m1() {
+        //condition
+    }
+
+}
