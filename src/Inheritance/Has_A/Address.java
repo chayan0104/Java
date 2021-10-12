feae6d0bed35bbe8f7d594bd111ecda3b9687ab2
chayan0104 <92307176+chayan0104@users.noreply.github.com>
2021-10-12 10:08:38 +0530

    oops

---
+++ src/Inheritance/Has_A/Address.java
@@ -0,0 +1,13 @@
+package Inheritance.Has_A;
+
+public class Address {
+    String city;
+    String State;
+    int pin;
+// constructor
+    public Address(String city, String state, int pin) {
+        this.city = city;
+        State = state;
+        this.pin = pin;
+    }
+}
---
+++ src/Inheritance/Has_A/Student.java
@@ -0,0 +1,6 @@
+package Inheritance.Has_A;
+//here Address class is reference as object ion student class
+public class Student {
+    String name;
+    Address A;
+}
