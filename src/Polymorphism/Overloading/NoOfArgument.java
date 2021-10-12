f1c5cdf7c9efcd9b1a844cc974efd8b8d91ee146
chayan0104 <92307176+chayan0104@users.noreply.github.com>
2021-10-12 10:41:41 +0530

    oops

---
+++ src/Polymorphism/Overloading/NoOfArgument.java
@@ -0,0 +1,17 @@
+package Polymorphism.Overloading;
+//changing the no of arguments
+class NoOfArgument {
+    static int add(int a,int b){ //1st method with 2 argument
+        return a+b;
+    }
+    static  int add (int a,int b ,int c){ //2nd method with 2 argument
+        return a+b+c;
+    }
+
+}
+class overloading{
+    public static void main(String[] args) {
+        System.out.println(NoOfArgument.add(11,15));
+        System.out.println(NoOfArgument.add(15,16,44));
+    }
+}
---
+++ src/Polymorphism/Overloading/TypeOfArgument.java
@@ -0,0 +1,16 @@
+package Polymorphism.Overloading;
+
+public class TypeOfArgument {
+    static int m1(int a, int b){
+        return a+b;
+    }
+    static double m1(double a, double b, double c){
+        return  a+b+c;
+    }
+}
+class Test1{
+    public static void main(String[] args) {
+        System.out.println(TypeOfArgument.m1(12,44));
+        System.out.println(TypeOfArgument.m1(22,58,74));
+    }
+}
