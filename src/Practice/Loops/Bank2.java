9efbc35822e80c023ccb78e6fe0113273b5a7ec8
chayan0104 <92307176+chayan0104@users.noreply.github.com>
2021-10-20 16:19:41 +0530

    oops

---
+++ src/Practice/Loops/Bank2.java
@@ -0,0 +1,43 @@
+package Practice.Loops;
+
+import java.util.Scanner;
+
+public class Bank2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter Operation");
+        System.out.println("Operations Are");
+        System.out.println("1: Take Money");
+        System.out.println("2: Put Money to Balance");
+        System.out.println("3: To Exit");
+
+     int operation = sc.nextInt();
+
+        while(true){
+           if (operation==1){
+               System.out.println("enter Balance");
+               int balance =sc.nextInt();
+               System.out.println("Enter money you want");
+               int money =sc.nextInt();
+                if(money>balance){
+                    System.out.println("You cannot do this operation");
+                   // continue; go back beginning o0f while loop.
+                    //break // breaks the iteration.
+
+                }
+                else{
+                    System.out.println("You got money: "+money);
+
+                }
+           }
+           else if(operation==2){
+               System.out.println("Enter your balance");
+               int balance = sc.nextInt();
+               System.out.println("enter the money you want To Put");
+               int put = sc.nextInt();
+           }
+
+
+        }
+    }
+}
--- src/Practice/OperatorAndStatements/Bank.java
+++ src/Practice/OperatorAndStatements/Bank.java
@@ -1,4 +1,78 @@
 package Practice.OperatorAndStatements;
 
+import java.util.Scanner;
+
 public class Bank {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Banking Operation");
+        System.out.println("Our operations");
+        System.out.println("Click 1 for exit");
+        System.out.println("click 2 for Money Transfer");
+        System.out.println("click 3 for get money");
+
+        int operation = sc.nextInt();// Specifying operation from user input
+
+        //Exit operation
+        if (operation == 1){
+            System.out.println("Exiting from the system");
+        }
+
+
+        //Transfer money
+        else if(operation==2){
+            System.out.println("how much money do you have?");
+            int balance= sc.nextInt();//100
+            System.out.println("how much money do you want to transfer");
+            int transfer =sc.nextInt();//50
+           /*  System.out.println("You have transferred Your money");
+            System.out.println("Your current Account Balance is: "+(balance-transfer));
+            System.out.println("Congratulations");*/
+
+            //validate
+            if(balance<transfer){
+                System.out.println("Sorry You do not have that much money");
+            }
+            else if(balance>transfer){
+                System.out.println("You transferred" +transfer);
+                System.out.println("Your current Account Balance is: "+(balance-transfer));
+            }
+            else if (balance==transfer){
+                System.out.println("You transferred" +transfer);
+                System.out.println("Your current Account Balance is: "+(balance-transfer));               ;
+            }
+        }
+
+        //Get Money
+        else if(operation==3){
+            System.out.println("Enter your balance");
+            int balance = sc.nextInt();
+            System.out.println("how much Money you want to take?");
+            int money = sc.nextInt();
+            /*System.out.println("Ypu have : "+money );
+            System.out.println("Your Current balance is: "+(balance-money));
+            */
+            //validate
+            if(balance<money){
+                System.out.println("Sorry You do not have that much money");
+            }
+            else if(balance>money){
+                System.out.println("You got money" +money);
+                System.out.println("Your current Account Balance is: "+(balance-money));
+            }
+            else if (balance==money){
+                System.out.println("Your Have all you money ");               ;
+            }
+
+        }
+
+        //invalid
+        else {
+            System.out.println("Invalid operation");
+        }
+
+    }
+
+
 }
